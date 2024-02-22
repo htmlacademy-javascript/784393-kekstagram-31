23457d7a5708ff47e5aa94f278f24a8c3999e24e
Константин <konkomen1@gmail.com>
2024-02-22 14:46:04 +0500

    Вторая часть задания с фунциями, полиндром

--- js/functions.js
+++ js/functions.js
@@ -16,3 +16,18 @@ function getStringLenght2(string, lengthString) {
 // console.log(getStringLenght2('проверяемая строка', 20));
 // console.log(getStringLenght2('проверяемая строка', 18));
 // console.log(getStringLenght2('проверяемая строка', 10));
+
+
+function checkIsPalindrome(string) {
+  let newString = '';
+
+  for(let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+
+  return newString.toLocaleLowerCase() === string.toLocaleLowerCase();
+
+}
+
+// console.log(checkIsPalindrome('Топот'));
+// console.log(checkIsPalindrome('Кекс'));
